c04eacc667ec6a47d6e686f279cd18873c6f014c
hmzhrkn <122343560+hmzhrkn@users.noreply.github.com>
2023-02-21 14:45:18 +0700

    Add files via upload

---
+++ PBO.py
@@ -0,0 +1,11 @@
+class segitiga:
+    def __init__(self,alas,tinggi):
+        self.alas=alas
+        self.tinggi=tinggi
+
+def get_luas(self):
+    return 0.5 * self.alas * self.tinggi
+
+segitiga1= segitiga(5,10)
+
+print('luas segitiga ;', segitiga1.get_luas())
---
+++ prosedural.py
@@ -0,0 +1,6 @@
+def hitung_luas():
+    alas = float(input('Masukan alas :'))
+    tinggi= float(input('masukan tinggi:'))
+    print('luas = ' , 0,5 * alas *tinggi)
+hitung_luas()
+hitung_luas()
